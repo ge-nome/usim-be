c0d70099c6d9356cebea9eef98f3b9d45fc577a4
Michael Raheem <abiolar544@gmail.com>
2022-07-01 18:19:27 +0100

    init commit

---
+++ app/Http/Controllers/API/StudentController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $student = Student::all()
+        ->where('mat_no', 'like', "%{$request->mat_no}%");
+        return response()->json([
+            'status'=>200,
+            'student'=>$student
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'surname'=>'required',
+            'firstname'=>'required',
+            'email'=>'required|email',
+            'phone'=>'required',
+            'passport'=>'required|mimes:jpg,png,jpeg|max:5048',
+        ]);
+
+        $imageFile = $request->firstname.$request->surname.'.'.$request->passport->extension();
+
+        $request->passport->move(public_path('storage/'), $imageFile);
+
+        $student = Student::create([
+            'surname'=>$request->input('surname'),
+            'firstname'=>$request->input('firstname'),
+            'othername'=>$request->input('othername'),
+            'dob'=>$request->input('dob'),
+            'phone'=>$request->input('phone'),
+            'email'=>$request->input('email'),
+            'school_id'=>$request->input('school_id'),
+            'course_id'=>$request->input('course_id'),
+            'level_id'=>$request->input('level_id'),
+            'passport'=>$imageFile,
+        ]);
+        $student->save();
+
+        return response()->json([
+            'status'=>200,
+            'message'=>"Registration Successful",
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_06_28_173053_create_students_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string("mat_no");
+            $table->string("surname");
+            $table->string("firstname");
+            $table->string("othername")->nullable();
+            $table->date("dob");
+            $table->string("phone")->unique();
+            $table->string("email")->unique();
+            $table->foreignId("school_id")->constrained();
+            $table->foreignId("course_id")->constrained();
+            $table->foreignId("level_id")->constrained();
+            $table->integer("status");
+            $table->text("passport");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
